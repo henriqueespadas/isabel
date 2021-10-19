b9798a02b904db6a54f3b525ab1e1ce38b0ac0f8
espadas <henriqueep@hotmail.com>
2021-10-19 13:52:40 -0300

    django install

---
+++ princess/core/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse('<h1 style="font-family: sans-serif">Isabel Project</h1>')
---
+++ princess/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for princess project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "princess.settings")
+
+application = get_wsgi_application()
